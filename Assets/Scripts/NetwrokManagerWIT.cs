f08fe9fb742aad02dedb8ff45e8fce8acd449ade
tearsofdespair <mealiv2004@gmail.com>
2025-04-12 06:48:30 +0300

    Made chat Mechanic

---
+++ Assets/Scripts/NetwrokManagerWIT.cs
@@ -0,0 +1,9 @@
+using Mirror;
+using UnityEngine;
+
+public class NetwrokManagerWIT : NetworkManager
+{
+    [SerializeField] private int minPlayers = 2;
+    [Scene] [SerializeField] private string menuScene = string.Empty;
+    
+}
---
+++ Assets/Tests/Chat/Scripts/ChatBehavior.cs
@@ -0,0 +1,59 @@
+using System;
+using Mirror;
+using TMPro;
+using UnityEngine;
+
+public class ChatBehavior : NetworkBehaviour
+{
+    [SerializeField] private GameObject chatUI = null;
+    [SerializeField] private TMP_Text chatText = null;
+    [SerializeField] private TMP_InputField inputField = null;
+
+    private static event Action<string> OnMessage;
+
+    public override void OnStartAuthority()
+    {
+        chatUI.SetActive(true);
+
+        OnMessage += HandleMessage;
+    }
+
+    [ClientCallback]
+    private void OnDestroy()
+    {
+        if (!isOwned)
+        {
+            return;
+        }
+        OnMessage -= HandleMessage;
+    }
+
+    private void HandleMessage(string message)
+    {
+        chatText.text += message;
+    }
+
+    [Client]
+    public void Send(string message)
+    {
+        if(!Input.GetKeyDown(KeyCode.Return)) {return;}
+        
+        if(string.IsNullOrWhiteSpace(message)) {return;}
+
+        CmdSendMessage(inputField.text);
+        
+        inputField.text = string.Empty;
+    }
+
+    [Command]
+    private void CmdSendMessage(string message)
+    {
+        RpcHandleMessage($"[{connectionToClient.connectionId}]: {message}]");
+    }
+
+    [ClientRpc]
+    private void RpcHandleMessage(string message)
+    {
+        OnMessage?.Invoke(message);
+    }
+}
