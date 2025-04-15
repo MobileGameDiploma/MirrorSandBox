f8d64eee508d0a8cbac3c1d667dcf3f099597a8a
tearsofdespair <mealiv2004@gmail.com>
2025-04-15 08:54:52 +0300

    Mae lobby system

---
+++ Assets/Scripts/NetworkLobbyManager.cs
@@ -0,0 +1,33 @@
+using Mirror;
+using UnityEngine;
+
+public class NetworkLobbyManager : NetworkRoomManager
+{
+    bool showStartButton;
+    
+    public override void OnRoomServerPlayersReady()
+    {
+        // calling the base method calls ServerChangeScene as soon as all players are in Ready state.
+        if (Utils.IsHeadless())
+        {
+            base.OnRoomServerPlayersReady();
+        }
+        else
+        {
+            showStartButton = true;
+        }
+    }
+
+    public override void OnGUI()
+    {
+        base.OnGUI();
+
+        if (allPlayersReady && showStartButton && GUI.Button(new Rect(150, 300, 120, 20), "START GAME"))
+        {
+            // set to false to hide it in the game scene
+            showStartButton = false;
+
+            ServerChangeScene(GameplayScene);
+        }
+    }
+}
